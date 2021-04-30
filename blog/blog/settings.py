a39a91be106d392d4959ced41cf700b1161e6526
Aller Augusto de Souza <allerasouza@gmail.com>
2021-04-30 06:44:05 -0300

    Change language and timezone

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
