9a31964e42f389daffd91cf3d92f899b38cafea5
Aller Augusto de Souza <allerasouza@gmail.com>
2021-05-03 05:20:14 -0300

    Add a TextChoices to BD

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -3,9 +3,9 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin): # Create customization class for Post
-    list_display = ['title', 'sub_title', 'full_name']
+    list_display = ['title', 'sub_title', 'full_name', 'categories']
     search_fields = ['title', 'sub_title']
-    fields = ('title', 'sub_title', 'content')
+    # fields = ('title', 'sub_title', 'content')
 
 # Register your models here.
 admin.site.register(Post, PostAdmin) # Apply customization to Post
--- blog/website/models.py
+++ blog/website/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+    )
     
     def __str__(self): # Return title instead of "Post object" on object's name
         return self.title
